2f53341be30f509be825131c97d8998c2c93421c
pocari <caffelattenonsugar@gmail.com>
2018-10-08 21:30:20 +0900

    テスト追加

--- test.sh
+++ test.sh
@@ -24,6 +24,7 @@ function expect() {
   fi
 }
 
+#--------------------------
 command="\
 1
 2
@@ -32,6 +33,7 @@ command="\
 "
 expect "$command" "3"
 
+#--------------------------
 command="\
 1
 2
@@ -40,11 +42,247 @@ command="\
 "
 expect "$command" "-1"
 
+#--------------------------
 command="\
 1
 dup
 .s
 "
 expect "$command" "1 1"
+
+#--------------------------
+command="\
+2
+3
+*
+.
+"
+expect "$command" "6"
+
+#--------------------------
+command="\
+10
+2
+/
+.
+"
+expect "$command" "5"
+
+#--------------------------
+command="\
+1
+2
+<
+.
+"
+expect "$command" "1"
+
+
+#--------------------------
+command="\
+2
+1
+<
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+2
+2
+=
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+2
+3
+=
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+2
+3
+swap
+.s
+"
+expect "$command" "3 2"
+
+#--------------------------
+command="\
+2
+3
+swap
+swap
+.s
+"
+expect "$command" "2 3"
+
+#--------------------------
+command="\
+1
+1
+and
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+0
+1
+and
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+1
+0
+and
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+0
+0
+and
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+0
+not
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+1
+not
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+1
+2
+3
+rot
+.s
+"
+expect "$command" "2 3 1"
+
+
+#--------------------------
+command="\
+1
+2
+3
+drop
+.s
+"
+expect "$command" "1 2"
+
+#--------------------------
+command="\
+mem
+3
+!
+2
+mem
+@
++
+.
+"
+expect "$command" "5"
+
+#--------------------------
+command="\
+1
+1
+or
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+0
+1
+or
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+1
+0
+or
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+0
+0
+or
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+1
+2
+>
+.
+"
+expect "$command" "0"
+
+#--------------------------
+command="\
+2
+1
+>
+.
+"
+expect "$command" "1"
+
+#--------------------------
+command="\
+: my_dup dup ;
+3
+dup
+.s
+"
+expect "$command" "3 3"
+
+#--------------------------
+command="\
+: my_command rot - + ;
+5
+3
+1
+my_command
+.
+"
+expect "$command" "-1"
+
 echo
 
